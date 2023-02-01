3376b470566544ef9cc20c2e5c92897e09488eca
Akshaya <aramanar@ford.com>
2023-02-01 10:24:26 -0500

    mapped type

--- TypeScript/AdvancedTypes/MappedTypes.ts
+++ TypeScript/AdvancedTypes/MappedTypes.ts
@@ -9,9 +9,14 @@ let empExample: employeeWithOptional = {
     id : 5
 }
 
-
+//apply optional to all the properties to the type
 type removeOptional<Type> = {
     [Property in keyof Type]-? : Type[Property]
 }
-
 type employee = removeOptional<employeeWithOptional>;
+
+//map all the properties as readOnly
+type makeReadOnly<Type> = {
+    readonly [Property in keyof Type] : Type[Property]
+}
+type employeeReadOnly = makeReadOnly<employeeWithOptional>;
